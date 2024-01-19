83ecd302e26ad22a4082a876de73b36504849c94
wey <pukaimakaola@gmail.com>
2024-01-18 20:28:55 -0500

    alert appears and does not submit if date is in the past

---
+++ front-end/src/dashboard/ConstraintViolated.js
@@ -0,0 +1,19 @@
+function ConstraintViolated({ constraints }) {
+  if (constraints.includes("invalidDate")) {
+    return (
+      <div className="alert alert-danger">
+        <p>Constraint violated, invalid date</p>
+      </div>
+    );
+  } else if (constraints.includes("tuesday")) {
+    return (
+      <div className="alert alert-danger">
+        <p>Constraint violated, cannot be a tuesday</p>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+}
+
+export default ConstraintViolated;
