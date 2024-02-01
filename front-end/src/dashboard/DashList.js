6a9813f20df100312335c73fb764399d37c380aa
wey <pukaimakaola@gmail.com>
2024-02-01 00:14:57 -0500

    passes all US6 frontend tests

--- front-end/src/dashboard/DashList.js
+++ front-end/src/dashboard/DashList.js
@@ -4,26 +4,35 @@ import { Link } from "react-router-dom";
 function DashList({ reservations }) {
   const eachReservation = reservations.map((reservation) => {
     const { reservation_id } = reservation;
+    const seatButton = (
+      <Link to={`/reservations/${reservation.reservation_id}/seat`}>
+        <button
+          href={`/reservations/${reservation.reservation_id}/seat`}
+          type="button"
+          className="btn btn-primary"
+        >
+          Seat
+        </button>
+      </Link>
+    );
+
     return (
       <div key={reservation_id}>
         <h5>
           {reservation.first_name} {reservation.last_name}
         </h5>
         <div>
-          <p>reservation date: {reservation.reservation_date}</p>
-          <p>reservation time: {reservation.reservation_time}</p>
-          <p>mobile number: {reservation.mobile_number}</p>
-          <p>number of people: {reservation.people}</p>
+          <p>
+            Reservation {reservation.reservation_date}{" "}
+            {reservation.reservation_time}
+          </p>
+          <p>Contact: {reservation.mobile_number}</p>
+          <p>Guests: {reservation.people}</p>
+          <p data-reservation-id-status={reservation.reservation_id}>
+            Status: {reservation.status}
+          </p>
         </div>
-        <Link to={`/reservations/${reservation.reservation_id}/seat`}>
-          <button
-            href={`/reservations/${reservation.reservation_id}/seat`}
-            type="button"
-            className="btn btn-primary"
-          >
-            Seat
-          </button>
-        </Link>
+        {reservation.status === "booked" ? seatButton : ""}
       </div>
     );
   });
