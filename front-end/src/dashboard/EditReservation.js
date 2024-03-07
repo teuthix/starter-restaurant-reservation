43c6860829503bc5073fdadf09d86b52154e632e
wey <pukaimakaola@gmail.com>
2024-03-07 00:45:31 -0500

    created ReservationForm that EditReservation and NewReservation uses

--- front-end/src/dashboard/EditReservation.js
+++ front-end/src/dashboard/EditReservation.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { getSingleReservation, editReservation } from "../utils/api";
+import ReservationForm from "./ReservationForm";
 
 function EditReservation() {
   const param = useParams();
   const history = useHistory();
+  const [showError, setShowError] = useState("");
+
   const [formData, setFormData] = useState({
     first_name: "",
     last_name: "",
@@ -27,7 +30,7 @@ function EditReservation() {
           people: reservation.people,
         });
       } catch (error) {
-        console.error(error);
+        setShowError(error);
       }
     }
     fetchReservations();
@@ -55,99 +58,13 @@ function EditReservation() {
   return (
     <>
       <h3>Edit Reservation</h3>
-      <form onSubmit={submitHandler} className="d-flex flex-column mx-4 pt-4">
-        <>
-          <label htmlFor="first_name">first name:</label>
-          <input
-            id="first_name"
-            type="text"
-            name="first_name"
-            className="mb-4"
-            value={formData.first_name}
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="last_name">last name:</label>
-          <input
-            id="last_name"
-            type="text"
-            name="last_name"
-            className="mb-4"
-            value={formData.last_name}
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="mobile_number">mobile number:</label>
-          <input
-            name="mobile_number"
-            type="text"
-            id="mobile_number"
-            className="mb-4"
-            value={formData.mobile_number}
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="reservation_date">reservation date:</label>
-          <input
-            name="reservation_date"
-            id="reservation_date"
-            type="date"
-            placeholder="YYYY-MM-DD"
-            pattern="\d{4}-\d{2}-\d{2}"
-            className="mb-4"
-            value={formData.reservation_date}
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="reservation_time">reservation time:</label>
-          <input
-            name="reservation_time"
-            type="time"
-            id="reservation_time"
-            placeholder="HH:MM"
-            pattern="[0-9]{2}:[0-9]{2}"
-            className="mb-4"
-            value={formData.reservation_time}
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="people">number of people:</label>
-          <input
-            name="people"
-            type="number"
-            min="1"
-            id="people"
-            className="mb-4"
-            value={formData.people}
-            onChange={handleNumberChange}
-            required
-          />
-        </>
-        <div className="d-flex">
-          <div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => history.goBack()}
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </form>
+      <ReservationForm
+        formData={formData}
+        submitHandler={submitHandler}
+        handleChange={handleChange}
+        handleNumberChange={handleNumberChange}
+        history={history}
+      />
     </>
   );
 }
--- front-end/src/dashboard/NewReservations.js
+++ front-end/src/dashboard/NewReservations.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createReservation } from "../utils/api";
+import ReservationForm from "./ReservationForm";
 
 function NewReservations({ setDate, reservations, setReservations }) {
   const initialForm = {
@@ -19,6 +20,7 @@ function NewReservations({ setDate, reservations, setReservations }) {
 
   const handleChange = ({ target }) => {
     setFormData({ ...formData, [target.name]: target.value });
+    // console.log(formData);
   };
 
   const handleNumberChange = ({ target }) => {
@@ -42,100 +44,13 @@ function NewReservations({ setDate, reservations, setReservations }) {
   return (
     <>
       <h4 className="mt-4">New Reservation</h4>
-      <form onSubmit={submitHandler} className="d-flex flex-column mx-4 pt-4">
-        <>
-          <label htmlFor="first_name">first name:</label>
-          <input
-            id="first_name"
-            type="text"
-            name="first_name"
-            className="mb-4"
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="last_name">last name:</label>
-          <input
-            id="last_name"
-            type="text"
-            name="last_name"
-            className="mb-4"
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="mobile_number">mobile number:</label>
-          <input
-            name="mobile_number"
-            type="text"
-            id="mobile_number"
-            className="mb-4"
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="reservation_date">reservation date:</label>
-          <input
-            name="reservation_date"
-            id="reservation_date"
-            type="date"
-            placeholder="YYYY-MM-DD"
-            pattern="\d{4}-\d{2}-\d{2}"
-            className="mb-4"
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="reservation_time">reservation time:</label>
-          <input
-            name="reservation_time"
-            type="time"
-            id="reservation_time"
-            placeholder="HH:MM"
-            pattern="[0-9]{2}:[0-9]{2}"
-            className="mb-4"
-            onChange={handleChange}
-            required
-          />
-        </>
-        <>
-          <label htmlFor="people">number of people:</label>
-          <input
-            name="people"
-            type="number"
-            min="1"
-            id="people"
-            className="mb-4"
-            onChange={handleNumberChange}
-            required
-          />
-        </>
-        {showError ? (
-          <div className="alert alert-danger">
-            <p>{showError.message}</p>
-          </div>
-        ) : (
-          ""
-        )}
-        <div className="d-flex">
-          <div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => history.goBack()}
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </form>
+      <ReservationForm
+        formData={formData}
+        submitHandler={submitHandler}
+        handleChange={handleChange}
+        handleNumberChange={handleNumberChange}
+        history={history}
+      />
     </>
   );
 }
