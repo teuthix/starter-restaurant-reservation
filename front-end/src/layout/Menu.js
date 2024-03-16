ccaff05bc458a8b1b0fb89be6c3a7b0744ee586b
wey <pukaimakaola@gmail.com>
2024-03-16 01:45:55 -0400

    fixed sidebar

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -11,36 +11,35 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <nav className="navbar navbar-dark align-items-start p-0">
-      <div className="container-fluid d-flex flex-row">
+      <div className="container-fluid d-flex">
         <Link className="navbar-brand d-flex sidebar-brand" to="/">
-          <div className="sidebar-brand-text mx-3 my-2">
+          <div className="sidebar-brand-text mx-5 my-2">
             <span>Periodic Tables</span>
           </div>
         </Link>
-        {/* <hr className="sidebar-divider my-0" /> */}
         <ul
           className="nav navbar-nav flex-row text-light"
           id="accordionSidebar"
         >
-          <li className="nav-item">
+          <li className="nav-item mx-5">
             <Link className="nav-link" to="/dashboard">
               <span className="oi oi-dashboard" />
               &nbsp;Dashboard
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item mx-5">
             <Link className="nav-link" to="/search">
               <span className="oi oi-magnifying-glass" />
               &nbsp;Search
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item  mx-5">
             <Link className="nav-link" to="/reservations/new">
               <span className="oi oi-plus" />
               &nbsp;New Reservation
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item  mx-5">
             <Link className="nav-link" to="/tables/new">
               <span className="oi oi-layers" />
               &nbsp;New Table
