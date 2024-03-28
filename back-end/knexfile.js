df3fc2caa471dde11d4f9ca076dbdd187e3a2e04
wey <pukaimakaola@gmail.com>
2024-03-28 18:47:59 -0400

    changed NODE_ENV

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const path = require("path");
 
 const {
-  NODE_ENV = "development",
+  NODE_ENV = "production",
   DATABASE_URL = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
